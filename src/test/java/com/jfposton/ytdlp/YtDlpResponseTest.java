94ea1fc5199a36eaa62bdf073277d25e3fbaeb15
bivashy <me@bivashy.dev>
2026-01-04 00:47:45 +0500

    Fix tests

--- src/test/java/com/jfposton/ytdlp/YtDlpResponseTest.java
+++ src/test/java/com/jfposton/ytdlp/YtDlpResponseTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,11 +12,14 @@ import com.jfposton.ytdlp.mapper.VideoInfo;
 
 public class YtDlpResponseTest {
 
-  @Test
-  public void getFormatsCanHandleLargerVideos() throws YtDlpException {
-    VideoInfo videoInfo = YtDlp.getVideoInfo("https://www.youtube.com/watch?v=jPTO3lcPpik");
-    assertEquals("jPTO3lcPpik", videoInfo.getId());
-    assertNotNull(videoInfo.getFormats());
-    assertFalse(videoInfo.getFormats().isEmpty());
-  }
+    @Test
+    public void getFormatsCanHandleLargerVideos() throws YtDlpException {
+        List<VideoInfo> videoInfos = YtDlp.getVideoInfo("https://www.youtube.com/watch?v=jPTO3lcPpik");
+        assertEquals(videoInfos.size(), 1);
+
+        VideoInfo videoInfo = videoInfos.get(0);
+        assertEquals("jPTO3lcPpik", videoInfo.getId());
+        assertNotNull(videoInfo.getFormats());
+        assertFalse(videoInfo.getFormats().isEmpty());
+    }
 }
